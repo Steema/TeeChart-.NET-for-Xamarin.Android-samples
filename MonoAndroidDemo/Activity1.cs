a9cbf62a5cd2238ca6a1191e856d5bc8eaaf2cd0
Yeray Alonso <yeray@steema.com>
2016-07-07 15:44:41 +0200

    Added NavigationDrawer and About page to the main activity Application icon changed

--- MonoAndroidDemo/Activity1.cs
+++ MonoAndroidDemo/Activity1.cs
@@ -1,6 +1,10 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Net;
 using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V4.Widget;
+using Android.Views;
 using Android.Widget;
 
 namespace MonoAndroidDemo
@@ -8,6 +12,10 @@ namespace MonoAndroidDemo
   [Activity(Name = "com.steema.teechart.xamarin.android.Activity1", Label = "Xamarin.Android Demo", MainLauncher = true, Icon = "@drawable/icon")]
   public class Activity1 : BaseActivity
   {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
     protected override int LayoutResource
     {
       get
@@ -20,9 +28,45 @@ namespace MonoAndroidDemo
     {
       base.OnCreate(savedInstanceState);
 
-      SupportActionBar.Title = "TeeChart for Xamarin.Android Demo";
-      SupportActionBar.SetDisplayHomeAsUpEnabled(false);
-      SupportActionBar.SetHomeButtonEnabled(false);
+      SupportActionBar.Title = "TeeChart";
+
+      drawerLayout = this.FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+
+      //Set hamburger items menu
+      SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.ic_menu);
+
+      //setup navigation view
+      navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+
+      //handle navigation
+      navigationView.NavigationItemSelected += (sender, e) =>
+      {
+        e.MenuItem.SetChecked(true);
+
+        switch (e.MenuItem.ItemId)
+        {
+          case Resource.Id.nav_newChart:
+            //To-Do
+            break;
+          case Resource.Id.nav_openChart:
+            //To-Do
+            break;
+          case Resource.Id.nav_web:
+            StartActivity(new Intent(Intent.ActionView, Uri.Parse("https://www.steema.com/")));
+            break;
+          case Resource.Id.nav_about:
+            StartActivity(new Intent(this, typeof(About)));
+            break;
+          case Resource.Id.nav_mail:
+            StartActivity(new Intent(Intent.ActionView, Uri.Parse("mailto:info@steema.com")));
+            break;
+          case Resource.Id.nav_call:
+            StartActivity(new Intent(Intent.ActionView, Uri.Parse("tel:+34 972 218 797")));
+            break;
+        }
+
+        drawerLayout.CloseDrawers();
+      };
 
       var seriesAdapter = new SeriesAdapter(this);
       var seriesListView = FindViewById<ListView>(Resource.Id.seriesListView);
@@ -40,6 +84,16 @@ namespace MonoAndroidDemo
       StartActivityForResult(myIntent, 1);      
     }
 
+    public override bool OnOptionsItemSelected(IMenuItem item)
+    {
+      switch (item.ItemId)
+      {
+        case Android.Resource.Id.Home:
+          drawerLayout.OpenDrawer(Android.Support.V4.View.GravityCompat.Start);
+          return true;
+      }
+      return base.OnOptionsItemSelected(item);
+    }
   }
 }
 
